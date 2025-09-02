24f5b1740ee104dd8f933993418c25664e6f9fec
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2025-09-02 11:11:12 +0530

    fix: Update Appwrite client configuration to provide default endpoint and project ID

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,8 +1,8 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client()
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID || '687930a2002d9c1615c9');
 
 const account = new Account(client);
 const databases = new Databases(client);
